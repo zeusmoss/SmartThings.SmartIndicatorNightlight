34231748adcf99d8ae6a8b4fd244f4aed7416787
NotoriousBDG <notoriousbdg@gmail.com>
2014-11-04 11:33:21 -0500

    Documentation updates

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ===========
 
 Copyright (c) 2014 [Brandon Gordon](https://github.com/notoriousbdg)
+   Original Author: SmartThings
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 in compliance with the License. You may obtain a copy of the License at:
@@ -11,3 +12,25 @@ in compliance with the License. You may obtain a copy of the License at:
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
 on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 for the specific language governing permissions and limitations under the License.
+
+
+##Overview
+
+This SmartApp will turn the indicator light on many Z-Wave light switches to a motion activated night light.  The indicator light will turn on when there is motion and the switch is off.  The indicator light will be off by default.
+
+This SmartApp was forked from the Smart Nightlight SmartApp and modified to work with the indicator light.
+
+##Install Procedure
+
+1. Create new SmartApps at https://graph.api.smartthings.com/ide/apps using the SmartApp at https://github.com/notoriousbdg/SmartThings.SmartIndicatorNightlight.
+2. Install the newly created SmartApp in the SmartThings mobile application.
+3. Configure the inputs to the SmartApp as prompted.
+4. Tap done.
+5. Enjoy your new Smart Indicator Nightlight1. Create new SmartApps at https://graph.api.smartthings.com/ide/apps using the SmartApps at https://github.com/notoriousbdg/SmartThings.AverageThings.
+
+##Revision History
+
+2014-11-04  v0.0.1  Initial release
+
+The latest version of this file can be found at:
+  https://github.com/notoriousbdg/SmartThings.SmartIndicatorNightlight
--- SmartIndicatorNightlight.smartapp.groovy
+++ SmartIndicatorNightlight.smartapp.groovy
@@ -1,9 +1,42 @@
 /**
- *  Smart Indicator Nightlight
+ *  Smart Indicator Nightlight SmartApp for SmartThings
  *
- *  Author: Brandon Gordon (Original Author: SmartThings)
+ *  Copyright (c) 2014 Brandon Gordon (https://github.com/notoriousbdg)
+ *    Original Author: SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Overview
+ *  ----------------
+ *  This SmartApp will turn the indicator light on many Z-Wave light switches to a motion activated night light.  The indicator 
+ *  light will turn on when there is motion and the switch is off.  The indicator light will be off by default.
+ *  
+ *  This SmartApp was forked from the Smart Nightlight SmartApp and modified to work with the indicator light.
+ *
+ *  Install Steps
+ *  ----------------
+ *  1. Create new SmartApps at https://graph.api.smartthings.com/ide/apps using the SmartApp at https://github.com/notoriousbdg/SmartThings.SmartIndicatorNightlight.
+ *  2. Install the newly created SmartApp in the SmartThings mobile application.
+ *  3. Configure the inputs to the SmartApp as prompted.
+ *  4. Tap done.
+ *  5. Enjoy your new Smart Indicator Nightlight
+ *
+ *  Revision History
+ *  ----------------
+ *  2014-11-04  v0.0.1  Initial release
+ *
+ *  The latest version of this file can be found at:
+ *    https://github.com/notoriousbdg/SmartThings.SmartIndicatorNightlight
  *
  */
+ 
 definition(
     name: "Smart Indicator Nightlight",
     namespace: "notoriousbdg",
